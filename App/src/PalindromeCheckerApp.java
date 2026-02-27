47f5bb107b3bd29c3f0ad69c735cd6ce77a29c8a
MonikaR24 <monikaramesh240507@gmail.com>
2026-02-27 14:40:23 +0530

    Welcome message

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,16 @@
+public class PalindromeCheckerApp {
+    /**
+     * /**
+     * Application entry point
+     * <p>
+     * This is a java palindrome Checker App Welcome message
+     *
+     * @author Developer
+     * @Version 1.0
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Palindrome Checker Management System\n" +
+                "Version : 1.0\n" +
+                "System initialized successfully.");
+    }
+}
